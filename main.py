9dfda0d59b28daf7515b2db2a04198721b75f2d0
Christoffer Rosenlind <christoffer.rosenlind@gmail.com>
2024-04-21 19:40:21 +0200

    txt

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 files = os.listdir("pdf/")
 if len(files) != 0:
     for file in files:
-        docx = file.replace("pdf", "docx")
-        pdfextract.create_txt("pdf/" + file, "txt/" + docx)
-elif len(files) == 0:
+        txt = file.replace("pdf", "txt")
+        pdfextract.create_txt("pdf/" + file, "txt/" + txt)
+else:
     print("No files in dir.")
