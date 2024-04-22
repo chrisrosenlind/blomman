46a2fe3b15d1cf410612a9a694b8d20a1bc0e519
Christoffer Rosenlind <christoffer.rosenlind@gmail.com>
2024-04-22 07:27:12 +0200

    Create both docx and txt

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ files = os.listdir("pdf/")
 if len(files) > 0:
     for file in files:
         txt = file.replace("pdf", "txt")
-        pdfextract.create_txt("pdf/" + file, "txt/" + txt)
+        docx = file.replace("pdf", "docx")
+        pdfextract.create_file("pdf/" + file, "txt/" + txt)
+        pdfextract.create_file("pdf/" + file, "docx/" + docx)
 else:
     print("No files in dir.")
